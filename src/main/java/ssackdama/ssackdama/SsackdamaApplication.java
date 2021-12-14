854e1551f77ba29180b2c237a1e348617329031f
SeungJooKim <seoungjoo01@gmail.com>
2021-12-14 11:12:52 +0900

    Add Project

---
+++ src/main/java/ssackdama/ssackdama/SsackdamaApplication.java
@@ -0,0 +1,13 @@
+package ssackdama.ssackdama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsackdamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsackdamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssackdama/ssackdama/SsackdamaApplicationTests.java
@@ -0,0 +1,13 @@
+package ssackdama.ssackdama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsackdamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
