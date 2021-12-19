cb2cf94f668c1c0b42321f24ed5b8e99d794487d
SeungJooKim <seoungjoo01@gmail.com>
2021-12-19 22:29:52 +0900

    Add File Structure
    - MVC 패턴에 맞게  파일 구조 수정 ( DOMAIN / CONTROLLER / SERVICE / MAPPER / 기타 )
    - 참고 예제 주석으로 추가

--- src/main/java/ssackdama/ssackdama/controller/MainController.java
+++ src/main/java/ssackdama/ssackdama/controller/MainController.java
@@ -16,3 +16,23 @@ public class MainController {
     }
 
 }
+
+/*
+@Controller   //해당 클래스를 컨트롤러로 동작하게 한다.
+public class BoardController {
+
+	@Autowired
+	private BoardService boardService;
+
+	@RequestMapping("/apple")    // 이 주소로 접속하면 이 메소드를 호출한다.(매핑한다.)
+	public ModelAndView openBoardList() throws Exception{
+		ModelAndView mv = new ModelAndView("/apple/boardList");
+
+		List<BoardDto> list = boardService.selectBoardList(); //서비스 호출!!!!
+		mv.addObject("list",list);
+
+		return mv;
+	}
+}
+
+ */
