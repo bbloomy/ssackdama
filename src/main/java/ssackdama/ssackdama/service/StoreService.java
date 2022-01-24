ab431f6b9df46df9206a488a7a7d75c159a9b852
SeungJooKim <seoungjoo01@gmail.com>
2022-01-25 00:29:02 +0900

    Add Store Service & Test

--- src/main/java/ssackdama/ssackdama/service/StoreService.java
+++ src/main/java/ssackdama/ssackdama/service/StoreService.java
@@ -1,2 +1,11 @@
-package ssackdama.ssackdama.service;public interface StoreService {
+package ssackdama.ssackdama.service;
+
+import ssackdama.ssackdama.domain.Store;
+
+import java.util.List;
+
+public interface StoreService {
+
+    public List<Store> getAllStores();
+
 }
